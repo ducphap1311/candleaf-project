e15a010089554ebbd7141d8269180792b2b2a6b1
unknown <hophap1311@gmail.com>
2022-11-30 15:37:59 +0700

    fix

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const connectDB = require('./db/connect')
 const errorHandler = require('./middleware/error-handler')
 const notFound = require('./middleware/not-found')
 const candleafsRouter = require('./routes/candleafs')
-const path = require('path')
 
 app.use(cors())
 app.get("/", (req, res) => {
@@ -23,12 +22,12 @@ app.use('/api/v1/candleafs', candleafsRouter);
 app.use(notFound)
 app.use(errorHandler)
 
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static("client/build"));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//     })
-// }
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
